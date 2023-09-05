bc94d5c100636cc1b4042ce52e57a2d3f379a24a
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-09-04 21:21:50 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -5,14 +5,16 @@ class Grupo:
 
     def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas 
-        self.listadoAlumnos = estudiantes 
+        self._asignaturas = asignaturas if asignaturas is not None else []
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
     def listadoAsignaturas(self, **args):
         for x in args.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista = None):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
